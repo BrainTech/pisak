953d5b3cdc4c47403223784118d9f3e937b6a383
Sasza Kijek <saszasasha@gmail.com>
2015-08-03 16:58:11 +0200

    add scripts to setup.py and change to setuptools as dist tool refs #2494

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='PISAK',
-      version='1.0',
-      description='Polish Integrational System for Alternative Communication',
-      long_description=''
-      author='doc.pisak.org/authors.html',
-      author_email='contact@pisak.org',
-      url='http://pisak.org/',
-      packages=['pisak', 'pisak.libs', 'pisak.libs.audio',
-                'pisak.libs.blog', 'pisak.libs.email', 'pisak.libs.movie',
-                'pisak.libs.paint', 'pisak.libs.speller',
-                'pisak.libs.symboler', 'pisak.libs.viewer'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: X11 Applications',
-          'Intended Audience :: End Users/Desktop',
-          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python',
-          'Topic :: Communications',
-          ],
+setup(
+    name = 'PISAK',
+    version = '1.0',
+    description = 'Polish Integrational System for Alternative Communication',
+    long_description = ''
+    author = 'doc.pisak.org/authors.html',
+    author_email = 'contact@pisak.org',
+    url = 'http://pisak.org/',
+    packages = find_packages() 
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Communications',
+    ],
+    scripts = ['bin/pisak', 'bin/pisak-audio', 'bin/pisak-blog',
+               'bin/pisak-email', 'bin/pisak-movie', 'bin/pisak-paint',
+               'bin/pisak-speller', 'bin/pisak-symboler', 'bin/pisak-viewer']
 )
