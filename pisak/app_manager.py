a0dc83f8f12a974e089c53d7e8e8cc683fc8ff4d
Sasza Kijek <saszasasha@gmail.com>
2015-08-03 16:33:07 +0200

    change the structure of PISAK dirs refs #2494

--- pisak/app_manager.py
+++ pisak/app_manager.py
@@ -171,12 +171,11 @@ class AppManager(Clutter.Actor,
             worker.start()
 
     def _do_run_app(self, app_exec):
-        cmd = ["python3", app_exec] + sys.argv[1:] + ["--child"]
+        cmd = [app_exec] + sys.argv[1:] + ["--child"]
         self.current_app = subprocess.Popen(cmd)
         self.current_app.wait()
         Clutter.threads_add_idle(0, self.maximize_panel, None)
         self.current_app = None
 
     def _get_exec_path(self, module_name):
-        return importlib.import_module(".".join(["pisak.apps",
-                                                 module_name])).EXEC_PATH
+        return 'pisak-' + str(module_name)
--- pisak/apps/symboler/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-"""
-Path to the symboler application executable.
-"""
-EXEC_PATH = os.path.join(os.path.split(__file__)[0], "app.py")
