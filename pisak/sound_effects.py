f3954e9e67aa273b8362ec059f62891f756bbef0
Sasza Kijek <saszasasha@gmail.com>
2015-08-17 15:52:03 +0200

    change the sound effects play to raw Gst - ends cairo-gtk-x11 bug refs #2511

--- pisak/sound_effects.py
+++ pisak/sound_effects.py
@@ -1,12 +1,14 @@
 """
 Sound effects player.
 """
-from gi.repository import ClutterGst
+from gi.repository import Gst
+from pisak import logger
 
+_LOG = logger.get_logger(__name__)
 
-class SoundEffectsPlayerNative:
+class SoundEffectsPlayer:
     def __init__(self, sounds_list):
-        ClutterGst.init()
+        Gst.init()
 
         self.sounds = {}
 
@@ -27,24 +29,21 @@ class SoundEffectsPlayerNative:
             # volumes[sound_name] = volume
             vec = []
             for i in range(CHANNELS_PER_FILE):
-                s = ClutterGst.VideoTexture()
-                s.set_filename(config['file_name'])
-                s.set_buffering_mode(ClutterGst.BufferingMode.DOWNLOAD)  # DOWNLOAD
-                vec.append(s)
+                playbin = Gst.ElementFactory.make('playbin')
+                playbin.set_property('uri', 'file://' + config['file_name'])
+                vec.append(playbin)
             self.sounds[sound_name] = tuple(vec)
             # print('xxx: ' + str(self.sounds))
 
     def play(self, sound_name):
         if sound_name in self.sounds:
-            self.sounds[sound_name][0].set_playing(True)
+            self.sounds[sound_name][0].set_state(Gst.State.READY)
+            self.sounds[sound_name][0].set_state(Gst.State.PLAYING)
         else:
-            print('sound not found')
+            _LOG.warning('Sound not found.')
 
     def set_volume(self, volume):
         pass
 
     def shutdown(self):
         pass
-
-
-SoundEffectsPlayer = SoundEffectsPlayerNative
