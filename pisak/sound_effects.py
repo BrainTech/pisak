9c381217089295c207057b57cfb52b1d91c146dc
Sasza Kijek <saszasasha@gmail.com>
2015-10-27 16:14:18 +0100

    add prop volume to SoundEffectPlayer refs #2686

--- pisak/sound_effects.py
+++ pisak/sound_effects.py
@@ -21,15 +21,14 @@ class SoundEffectsPlayer(object):
     def __init__(self, sounds_dict):
         super().__init__()
         self.sounds = sounds_dict
+        self.volume = pisak.config.as_int('sound_effects_volume') / 100
         self._playbin = Gst.ElementFactory.make('playbin', 'button_sound')
-        self._playbin.set_property("volume",
-                                   pisak.config.as_int('sound_effects_volume') / 100)
+        self._playbin.set_property("volume", self.volume)
         self._bus = self._playbin.get_bus()
         self._bus.connect('message', self.on_message)
 
     def play(self, sound_name):
-        self._playbin.set_property("volume",
-                                   pisak.config.as_int('sound_effects_volume') / 100)
+        self._playbin.set_property("volume", self.volume)
         self._playbin.set_state(Gst.State.READY)
         if sound_name in self.sounds:
             self._playbin.set_property('uri', 'file://' + self.sounds[sound_name])
