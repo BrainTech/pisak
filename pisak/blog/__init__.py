db129ba627d1821e61ff3acd245a344e6117bf5f
Bartosz Jura <bjura@braintech.pl>
2015-11-05 21:12:22 +0100

    Timeout for blog test refs #2753

--- pisak/blog/__init__.py
+++ pisak/blog/__init__.py
@@ -2,13 +2,17 @@ def test(config):
     """
     Test provided blog settings or server connection.
     """
+    import socket
+
     from pisak.loc import BLOG_MESSAGES as MESSAGES
     from pisak.blog.wordpress import _OwnBlog
     from pisak.blog import exceptions
 
+    socket.setdefaulttimeout(5)
+
     try:
         dummy_blog = _OwnBlog(config)
-    except exceptions.BlogInternetError:
+    except (socket.timeout, exceptions.BlogInternetError):
         ret = False, MESSAGES['no_internet']
     except exceptions.BlogAuthenticationError:
         ret = False, MESSAGES['invalid_credentials']
@@ -16,4 +20,7 @@ def test(config):
         ret = False, MESSAGES['unknown']
     else:
         ret = True, MESSAGES['success']
+
+    socket.setdefaulttimeout(None)
+
     return ret
