e99236f450d7c48c28e53e9d866d1bd3f5af34d4
Alex Khrabrov <alex@mroja.net>
2016-03-26 13:57:42 +0100

    updates to installer script (wip)

--- install_pisak.sh
+++ install_pisak.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+#### check if current system is supported ####
+
+command -v lsb_release >/dev/null 2>&1 || { echo >&2 "\"lsb_release\" program not found. Are you using Ubuntu Linux?"; exit 1; }
+
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+
+DISTRIBUTOR=`lsb_release -is`
+# echo "Distributor is '$DISTRIBUTOR'."
+
+if [ "$DISTRIBUTOR" != "Ubuntu" ]; then
+   echo "This script supports only Ubuntu Linux."
+   echo "Your system distributor is: '$DISTRIBUTOR'."
+   exit 1
+fi
+
+DIST_CODENAME=`lsb_release -cs`
+#echo "Distribution codename is '$DIST_CODENAME'."
+
+DIST_VERSION=`lsb_release -sr`
+#echo "Distribution version is '$DIST_VERSION'."
+
+#### check if current system is supported ####
+
 # sudo add-apt-repository ppa:ethanak/milena
 
 sudo apt-get update && sudo apt-get upgrade -y
@@ -12,11 +35,26 @@ sudo apt-get install -y gir1.2-gst-plugins-base-0.10 gir1.2-gst-plugins-base-1.0
 
 sudo apt-get install -y python3 python3-gi python3-pil python3-gi-cairo python3-configobj python3-sqlalchemy python3-magic python3-pip python3-bs4 python3-ws4py python3-taglib python3-configobj python3-requests python3-pyqt5 python3-cssutils
 
-# sudo apt-get install -y milena
+# install milena package if available
+MILENA_PACK=(apt-cache policy milena)
+if [ -n $MILENA_PACK ] ;
+then
+    sudo apt-get install -y milena
+else
+    echo "'milena' package is not available. Skipping..."
+fi
 
-sudo pip3 install pressagio pydenticon ezodf python-wordpress-xmlrpc
+#### some Python packages are not available as Ubuntu deb packages ####
 
-sudo pip3 install --pre pyusb
+pip3 install --user pressagio pydenticon ezodf python-wordpress-xmlrpc
+
+if version_gt $DIST_VERSION "15.10"; then
+    sudo apt-get install python3-usb
+else
+    pip3 install --user --pre pyusb
+fi
+
+#### download pisak sources ####
 
 if [ -d ~/pisak ]; then
    echo "Directory ~/pisak exists."
